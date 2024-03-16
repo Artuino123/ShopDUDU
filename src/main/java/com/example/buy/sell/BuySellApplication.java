5e5efdeb08c629699453c58250c71aad80f0f9f8
Artur <artur_mikhaylov_1997@mail.ru>
2024-03-17 01:07:08 +0300

    Создал приложение на Spring Boot

---
+++ src/main/java/com/example/buy/sell/BuySellApplication.java
@@ -0,0 +1,12 @@
+package com.example.buy.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellApplication.class, args);
+	}
+}
