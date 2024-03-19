bc61b7cd2a4bff4c77788e299ed931f259fc114c
Artur <artur_mikhaylov_1997@mail.ru>
2024-03-19 21:59:37 +0300

    реализовал загрузку фото товара

--- src/main/java/com/example/shop/dudu/model/Product.java
+++ src/main/java/com/example/shop/dudu/model/Product.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Data
@@ -23,4 +27,17 @@ public class Product {
     private int price;
     @Column(name = "city")
     private String city;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
